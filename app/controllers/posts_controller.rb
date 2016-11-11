c071a42c14f39fd512092470c899fb54e70ce40e
Allu Balakrishna <balakrishna@carmatec.com>
2016-11-11 12:02:22 +0530

    Gon Sample

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -128,12 +128,12 @@ class PostsController < ApplicationController
   # GET /posts.json
   def index
     @posts = Post.all
+    gon.posts = @posts
   end
 
   # GET /posts/1
   # GET /posts/1.json
   def show
-    byebug
   end
 
   # GET /posts/new
---
+++ app/views/comments/_comment.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! comment, :id, :co, :created_at, :updated_at
+json.url comment_url(comment, format: :json)
